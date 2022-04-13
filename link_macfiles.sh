a40528083f742f3bf2f30e4f7641f123eeecc792
Llandy Riveron Del Risco <llandy3d@gmail.com>
2022-04-13 02:02:23 +0200

    alacritty

--- link_macfiles.sh
+++ link_macfiles.sh
@@ -4,6 +4,7 @@
 ln -s $(pwd)/init.vim $HOME/.config/nvim
 ln -s $(pwd)/python.vim $HOME/.vim/after/ftplugin
 ln -s $(pwd)/config.fish $HOME/.config/fish
+ln -s $(pwd)/alacritty.yml $HOME/.config/alacritty
 ln -s $(pwd)/.vimrc $HOME
 ln -s $(pwd)/.skhdrc $HOME
 ln -s $(pwd)/.yabairc $HOME
