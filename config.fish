11c36d2bb52dfbd27a37a0c9cd0a33c4b9536771
Llandy Riveron Del Risco <llandy3d@gmail.com>
2024-05-18 12:37:30 +0200

    add go path

--- config.fish
+++ config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     fish_vi_key_bindings
     fish_add_path ~/opt/venv/bin
     fish_add_path ~/.local/bin
+    fish_add_path ~/go/bin
 
     # aliases
     alias ll='ls -al'
